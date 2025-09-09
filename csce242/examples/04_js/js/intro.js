0fe398709501068c84ceb0121f33a30e61b8b205
ddobbinss <damiendobbins31@gmail.com>
2025-09-09 12:53:08 -0400

    inclass js intro

---
+++ csce242/examples/04_js/js/intro.js
@@ -0,0 +1,14 @@
+/*
+
+const sayHello = () => {
+    console.log("hello world");
+}
+
+
+document.getElementById(btnClickMe).onclick = sayHello;
+
+*/
+
+document.getElementById("btn-click-me").onclick = () => {
+    console.log("hello world")
+};
