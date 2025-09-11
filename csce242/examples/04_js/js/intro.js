a999640ec95d32d28cdab9cb5e932f7150c6379f
ddobbinss <damiendobbins31@gmail.com>
2025-09-11 12:52:41 -0400

    in class javascript exmaple

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -9,6 +9,25 @@ document.getElementById(btnClickMe).onclick = sayHello;
 
 */
 
-document.getElementById("btn-click-me").onclick = () => {
-    console.log("hello world")
+document.getElementById("btn-sad").onclick = (event) => {
+    document.getElementById("p-feeling").innerHTML = "Doom is Near";
+    //document.getElementById("btn-click-me").classList.add("clicked");
+    event.currentTarget.classList.add("sad-clicked"); //current target is thebutton that was pressed
+    
+};
+
+document.getElementById("btn-happy").onclick = (event) => {
+    document.getElementById("p-feeling").innerHTML = "SunShine and Rainbows!";
+    event.currentTarget.classList.add("happy-clicked");
+}
+
+document.getElementById("btn-clear").onclick = (event) => {
+    document.getElementById("p-feeling").innerHTML = "Cleared";
+    
+}
+
+document.getElementById("txt-emotion").onkeyup = (event) => {
+    const userInput = event.currentTarget.value;
+    document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}`
+    document.getElementById("img-emotion").classList.remove("hidden");
 };
